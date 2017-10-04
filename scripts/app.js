65865b382700e037493e3d1d8e0a3151c60f0c74
Your Name <you@example.com>
2017-10-04 12:30:17 -0700

    got the data from api, got hhe google map to the website

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,42 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 
 $(document).ready(function() {
   console.log("Let's get coding!");
-  // CODE IN HERE!
 
+  // CODE IN HERE!
+	  $.ajax({
+		  method: 'GET',
+		  url: weekly_quakes_endpoint,
+		  // data:
+		  success: onSuccess,
+		  error: onError, 
+	  });
+	      function initMap() {
+        var sanFrancisco = { lat: 37.78, lng: -122.44}
+        
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: sanFrancisco
+        });
+        var marker = new google.maps.Marker({
+          position: sanFrancisco,
+          map: map
+        });
+      }
+	initMap();
 });
+
+  function onSuccess(responseData){
+  	console.log(responseData )
+  	responseData.features.forEach(function(name){
+  		$('#info').append(`<p>${name.properties.title}</p>`);
+  		
+
+  	})
+
+  }
+
+  function onError(){
+  	console.log("error")
+  }
+
+
